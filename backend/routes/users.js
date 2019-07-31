d87a277c1f16755474d6b0bae7e87afa3f2b5222
Mohit Kacha <kachamohit3@gmail.com>
2019-07-31 14:05:40 -0300

    added photographer profile update

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,120 +7,113 @@ var config = require('./../config');
 
 var User = require('./../models/users');
 
-router.post('/user/sign-up', async (req,res)=>{ 
+
+router.post('/user/sign-up', async (req, res) => {
     console.log('\n req body : ', req.body)
-  var schema = {
-  'name': {
-      notEmpty: true,
-      errorMessage: "Name is required"
-  },
-  'email': {
-      notEmpty: true,
-      errorMessage: "Email is required"
-  },
-  'password': {
-      notEmpty: true,
-      errorMessage: "Password is required"
-  },
-  'user_role': {
-      notEmpty: true,
-      errorMessage: "user_role is required"
-  }
-};
-req.checkBody(schema);
-var errors = req.validationErrors();
-console.log('\n Err : ', errors);
-if (!errors) {
-  var usercheck = await User.findOne({ email: req.body.email, isDeleted: false, user_role: req.body.user_role });
-  console.log('usercheck====>', usercheck);
-  if (usercheck) {
-      res.status(config.OK_STATUS).json({
-          status: 'failed',
-          message: "Email is already exist!!"
-      });
-  } else {
-    var userModel = new User(req.body);
-    userModel.save((err, userData)=> {
-      userData = JSON.parse(JSON.stringify(userData));
-      console.log("data:", userData);
-      if (err) {
-          res.status(config.BAD_REQUEST).json({
-              status: 'failed',
-              message: "could not register user please try again!!"
-          });
-      } else {
-          var token = jwt.sign({ id: userData._id, user_role: userData.user_role }, config.ACCESS_TOKEN_SECRET_KEY, {
-              expiresIn: 60 * 60 * 24 // expires in 24 hours
-          });
-          delete userData.password;
-          delete userData.isDeleted;
+    var schema = {
+        'name': {
+            notEmpty: true,
+            errorMessage: "Name is required"
+        },
+        'email': {
+            notEmpty: true,
+            errorMessage: "Email is required"
+        },
+        'password': {
+            notEmpty: true,
+            errorMessage: "Password is required"
+        },
+        'user_role': {
+            notEmpty: true,
+            errorMessage: "user_role is required"
+        }
+    };
+    req.checkBody(schema);
+    var errors = req.validationErrors();
+    console.log('\n Err : ', errors);
+    if (!errors) {
+        var usercheck = await User.findOne({ email: req.body.email, isDeleted: false, user_role: req.body.user_role });
+        console.log('usercheck====>', usercheck);
+        if (usercheck) {
+            res.status(config.OK_STATUS).json({
+                status: 'failed',
+                message: "Email is already exist!!"
+            });
+        } else {
+            var userModel = new User(req.body);
+            userModel.save((err, userData) => {
+                userData = JSON.parse(JSON.stringify(userData));
+                console.log("data:", userData);
+                if (err) {
+                    res.status(config.BAD_REQUEST).json({
+                        status: 'failed',
+                        message: "could not register user please try again!!"
+                    });
+                } else {
+                    delete userData.password;
+                    delete userData.isDeleted;
 
-          var result = {
-              status: 'success',
-              message: "User registered successfully.",
-              data: { user: userData },
-              token: token
-          };
-          res.status(config.OK_STATUS).json(result);
-      }
-  });
-  }
-} else {
-  res.status(config.BAD_REQUEST).json({
-      status: 'failed',
-      message: "Validation Error",
-      errors
-  });
-}
+                    var result = {
+                        status: 'success',
+                        message: "User registered successfully.",
+                        data: userData
+                    };
+                    res.status(config.OK_STATUS).json(result);
+                }
+            });
+        }
+    } else {
+        res.status(config.BAD_REQUEST).json({
+            status: 'failed',
+            message: "Validation Error",
+            errors
+        });
+    }
 
-});
-
-router.get('/user', async (req,res)=>{
-    res.json({'test':"riddhi"});
 });
 
 //route for logging in a user
-router.post('/login', async(req, res, next)=>{    
-        User.findOne(
-            { email: req.body.username, isDeleted: false },
-            function(err, user) {
-                if (err) throw err;
-                if (user) {
+router.post('/login', async (req, res, next) => {
+    User.findOne(
+        { email: req.body.username, isDeleted: false },
+        function (err, user) {
+            if (err) throw err;
+            if (user) {
 
-                    // ---------- IF ENCRYPTION ------------------
-                    if (bcrypt.compare(req.body.password, user.password, function(err, isMatch) {
-                        if (err) throw err;
-                        res.json(user);
-                    }));
+                // ---------- IF ENCRYPTION ------------------
+                if (bcrypt.compare(req.body.password, user.password, function (err, isMatch) {
+                    if (err) throw err;
+                    res.json(user);
+                }));
 
-                    // ----------- IF NO ENCRYPTION -------------------------
-                    /*var passwordCompare = user.password;
-                    if (passwordCompare === req.body.password) {
-                        res.json(user)
-                    } */                   
-                   /* else {
-                        res.status(404).send("Invalid login info.");
-                    }*/                   
-                } else {
-                    console.log(req.body);
-                    res.status(404).send("Invalid login info")
-                }
+                // ----------- IF NO ENCRYPTION -------------------------
+                /*var passwordCompare = user.password;
+                if (passwordCompare === req.body.password) {
+                    res.json(user)
+                } */
+                /* else {
+                     res.status(404).send("Invalid login info.");
+                 }*/
+            } else {
+                console.log(req.body);
+                res.status(404).send("Invalid login info")
             }
-        )
-    
+        }
+    )
+
 })
 
 //route for updating a user
-router.post('/updateUser', async(req, res, next)=>{    
-    
-    
-    User.findOneAndUpdate({"_id" : req.body._id},
-                {$set: { "name": req.body.usersName, "email": req.body.usersEmail }}, 
-                {new: true}, (err, user) => {
-                    if (err) throw err;
-                    res.json(user);
-                });
-                
+router.post('/updateUser', async (req, res, next) => {
+
+
+    User.findOneAndUpdate({ "_id": req.body._id },
+        { $set: { "name": req.body.usersName, "email": req.body.usersEmail } },
+        { new: true }, (err, user) => {
+            if (err) throw err;
+            res.json(user);
+        });
+
     /*User.findOne(
         { email: req.body.username, isDeleted: false },
         function(err, user) {
@@ -136,5 +129,36 @@ router.post('/updateUser', async(req, res, next)=>{
 
 })
 
+router.post('/user/profile', async (req, res) => {
+    var schema = {
+        'user_id': {
+            notEmpty: true,
+            errorMessage: "user_id is required"
+        }
+    };
+    req.checkBody(schema);
+    var errors = req.validationErrors();
+    if (!errors) {
+        var check = await User.findOne({ "_id": req.body.user_id, "isDeleted": false }).exec();
+        if (check) {
+            User.update({ "_id": req.body.user_id }, { $set: req.body }, async function (err, response) {
+                if (err) {
+                    return next(err);
+                } else {
+                    const data = await User.findOne({ "_id": req.body.user_id }).exec();
+                    console.log('\n data', data);
+                    res.status(config.OK_STATUS).json({ status: "success", message: "Profile updated successfully", data: data });
+                }
+            });
+        } else {
+            res.status(config.OK_STATUS).json({ status: "failed", message: "Record not found" });
+        }
+    } else {
+        res.status(config.BAD_REQUEST).json({
+            message: "Validation Error",
+            error: errors
+        });
+    }
+});
+
 module.exports = router;
- 
